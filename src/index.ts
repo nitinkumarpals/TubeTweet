ff829fdef898f6d4fd360c4e18b8908c06f4130b
nitinkumarpals <nitinpal900@gmail.com>
2024-10-06 14:57:55 +0530

    feat(db): connected to MongoDB instance

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express,{Response, Request} from "express";
+import connectDb from "./db";
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.use(express.json());
-
+connectDb();
 app.use("/", (req: Request, res: Response) => {
   res.status(200).json({
     message: "hello bun",
