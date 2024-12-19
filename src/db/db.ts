c27807a2335ee4af0ea840c48bdfe2b7d46a64ec
nitinkumarpals <nitinpal900@gmail.com>
2024-12-19 20:08:51 +0530

    Fixed connection bug

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,24 +1,18 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants";
-
-const connectDb = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(
-            `${process.env.MONGODB_URI}/${DB_NAME}`
-        );
-        console.log(
-            `\n MongoDb Connected !! DB HOST: ${connectionInstance.connection.host}`
-        );
-        //${connectionInstance.connection.host} this will give me url of mongodb
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            console.error(`MongoDb connection FAILED:`, error);
+const connectDb = async() => {
+    mongoose
+        .connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        .then((connectionInstance) => {
+            console.log(
+                "Connected to MongoDB:",
+                connectionInstance.connection.host
+            );
+        })
+        .catch((error) => {
+            console.error("MongoDB connection error:", error);
             console.error(error.stack);
-        } else {
-            console.error(`MongoDb connection FAILED:  ${error}`);
-        }
-        process.exit(1);
-    }
+            process.exit(1);
+        });
 };
-
 export default connectDb;
